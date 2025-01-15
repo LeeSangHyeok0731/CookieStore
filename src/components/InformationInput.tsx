e665113b06ec668dff146e33d10de6d52592e067
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-01-15 22:58:41 +0900

    정보 쿠키 입력 코드 추가

--- src/components/InformationInput.tsx
+++ src/components/InformationInput.tsx
@@ -1,3 +1,31 @@
+import { useState } from "react";
+import { setCookie } from "./CookieStore";
+
 export default function InformationInput() {
-  return <></>;
+  const [name, setName] = useState<string>("");
+  const [value, setValue] = useState<string>("");
+
+  const handleClick = () => {
+    setCookie(name, value);
+  };
+
+  return (
+    <>
+      <input
+        placeholder="이름"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      ></input>
+      <input
+        placeholder="값"
+        value={value}
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      ></input>
+      <button onClick={handleClick}>저장</button>
+    </>
+  );
 }
