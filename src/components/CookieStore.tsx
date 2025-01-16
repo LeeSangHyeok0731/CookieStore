e0e140efd31ee2b47a82f2a17c4d05f7d36a7db9
LeeSangHyeok0731 <s24066@gsm.hs.kr>
2025-01-16 17:53:06 +0900

    쿠키 삭제 & 모든 쿠키 불러오기

--- src/components/CookieStore.tsx
+++ src/components/CookieStore.tsx
@@ -3,10 +3,17 @@ import { Cookies } from "react-cookie";
 const cookies = new Cookies();
 
 export const setCookie = (name: string, value: string, options?: any) => {
-  console.log(name, value);
   return cookies.set(name, value, options);
 };
 
 export const getCookie = (name: string) => {
   return cookies.get(name);
+};
+
+export const getAllCookie = () => {
+  return cookies.getAll();
+};
+
+export const deleteCookie = (name: string) => {
+  return cookies.remove(name);
 };
